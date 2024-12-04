9a57fb1e33845e9202de5a8a7b7f38daabe17ab5
devindeng94 <2430070746@qq.com>
2024-12-04 17:54:16 +0800

    update 2s3z

--- smac/smac/env/scripts/__init__.py
+++ smac/smac/env/scripts/__init__.py
@@ -3,6 +3,10 @@ from .s_8m.script import DecisionTreeScript as DTS_8m
 from .s_8m.script_1 import DecisionTreeScript as DTS_8m_1
 from .s_8m.script_2 import DecisionTreeScript as DTS_8m_2
 from .s_27m.script import DecisionTreeScript as DTS_27m
+
+from .s_2s3z.script import DecisionTreeScript as DTS_2s3z
+from .s_2s3z.script_1 import DecisionTreeScript as DTS_2s3z_1
+
 from .s_3s5z.script import DecisionTreeScript as DTS_3s5z
 from .s_3s5z.script_1 import DecisionTreeScript as DTS_3s5z_1
 from .s_3s_vs_5z.script import DecisionTreeScript as DTS_3s_vs_5z
@@ -36,7 +40,7 @@ SCRIPT_DICT = {
     '25m': [DTS_27m],
     '27m_vs_30m': [DTS_27m],
     '3s5z': [DTS_3s5z, DTS_3s5z_1],
-    '2s3z': [DTS_3s5z, DTS_3s5z_1],
+    '2s3z': [DTS_2s3z, DTS_2s3z_1],
     '3s5z_vs_3s6z': [DTS_3s5z],
     '3s_vs_3z': [DTS_3s_vs_5z],
     '3s_vs_4z': [DTS_3s_vs_5z],
